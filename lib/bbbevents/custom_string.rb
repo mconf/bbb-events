1603e6dea47ad1142ef75f4076ed18d38afb3047
Felipe Cecagno <fcecagno@gmail.com>
2023-04-20 00:02:31 -0300

    fix: underscore function for strings

--- lib/bbbevents/custom_string.rb
+++ lib/bbbevents/custom_string.rb
@@ -1,7 +1,11 @@
 class String
   # ruby mutation methods have the expectation to return self if a mutation occurred, nil otherwise. (see http://www.ruby-doc.org/core-1.9.3/String.html#method-i-gsub-21)
+  # https://github.com/wycats/rails-api/blob/master/vendor/rails/activesupport/lib/active_support/inflector/methods.rb#L38-L46
   def underscore!
-    gsub!(/(.)([A-Z])/,'\1_\2')
+    gsub!(/::/, '/')
+    gsub!(/([A-Z]+)([A-Z][a-z])/,'\1_\2')
+    gsub!(/([a-z\d])([A-Z])/,'\1_\2')
+    tr!("-", "_")
     downcase!
   end
 
